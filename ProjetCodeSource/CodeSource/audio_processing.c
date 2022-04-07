8572c578809875084c0489d981a72742ce9b4fdf
Bassam El Rawas <bassamr99@gmail.com>
2022-04-07 11:24:05 +0200

    Add initial files

---
+++ ProjetCodeSource/CodeSource/audio_processing.c
@@ -0,0 +1,140 @@
+#include "ch.h"
+#include "hal.h"
+#include <main.h>
+#include <usbcfg.h>
+#include <chprintf.h>
+
+#include <motors.h>
+#include <audio/microphone.h>
+#include <audio_processing.h>
+#include <communications.h>
+#include <fft.h>
+#include <arm_math.h>
+
+//semaphore
+static BSEMAPHORE_DECL(sendToComputer_sem, TRUE);
+
+//2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
+static float micLeft_cmplx_input[2 * FFT_SIZE];
+static float micRight_cmplx_input[2 * FFT_SIZE];
+static float micFront_cmplx_input[2 * FFT_SIZE];
+static float micBack_cmplx_input[2 * FFT_SIZE];
+//Arrays containing the computed magnitude of the complex numbers
+static float micLeft_output[FFT_SIZE];
+static float micRight_output[FFT_SIZE];
+static float micFront_output[FFT_SIZE];
+static float micBack_output[FFT_SIZE];
+
+/*
+*	Callback called when the demodulation of the four microphones is done.
+*	We get 160 samples per mic every 10ms (16kHz)
+*	
+*	params :
+*	int16_t *data			Buffer containing 4 times 160 samples. the samples are sorted by micro
+*							so we have [micRight1, micLeft1, micBack1, micFront1, micRight2, etc...]
+*	uint16_t num_samples	Tells how many data we get in total (should always be 640)
+*/
+
+static int positionInBuffer = 0;
+static int sendToComputer = 0;
+
+void processAudioData(int16_t *data, uint16_t num_samples) {
+
+	/*
+	*
+	*	We get 160 samples per mic every 10ms
+	*	So we fill the samples buffers to reach
+	*	1024 samples, then we compute the FFTs.
+	*
+	*/
+
+	for(int i = 0; i < (int)num_samples/4; ++i) {
+		micRight_cmplx_input[positionInBuffer] = data[4*i];
+		micLeft_cmplx_input[positionInBuffer] = data[4*i + 1];
+		micBack_cmplx_input[positionInBuffer] = data[4*i + 2];
+		micFront_cmplx_input[positionInBuffer] = data[4*i + 3];
+
+		micRight_cmplx_input[positionInBuffer + 1] = 0; //imaginary part = 0
+		micLeft_cmplx_input[positionInBuffer + 1] = 0;
+		micBack_cmplx_input[positionInBuffer + 1] = 0;
+		micFront_cmplx_input[positionInBuffer + 1] = 0;
+
+		positionInBuffer += 2;
+
+		if(positionInBuffer >= 2*FFT_SIZE) break; //finished filling up the 2*1024 buffer
+	}
+
+	if(positionInBuffer >= 2*FFT_SIZE) {
+		//reset positionInBuffer
+		positionInBuffer = 0;
+
+		//compute FFT
+		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
+
+		//compute magnitude of FFT
+		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
+		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
+
+		//send to computer
+		if(sendToComputer == 5) {
+			chBSemSignal(&sendToComputer_sem);
+			sendToComputer = 0;
+
+//			int maxLeftOutput = 0;
+//			float maxFreq = 0;
+//			for(int i = 0; i < FFT_SIZE; ++i) {
+//				if(micLeft_output[i] > maxLeftOutput) {
+//					maxLeftOutput = micLeft_output[i]; //this will contain the biggest amplitude of the FFT
+//					maxFreq = i;
+//				}
+//			}
+//
+//			float newFreq = 15365 - maxFreq * 15; //linear transformation to correct
+//
+//			//chprintf((BaseSequentialStream *)&SDU1, "%nMaxFreq=%.2f \r\n", newFreq);
+
+		} else {
+			++sendToComputer;
+		}
+	}
+
+}
+
+void wait_send_to_computer(void){
+	chBSemWait(&sendToComputer_sem);
+}
+
+float* get_audio_buffer_ptr(BUFFER_NAME_t name){
+	if(name == LEFT_CMPLX_INPUT){
+		return micLeft_cmplx_input;
+	}
+	else if (name == RIGHT_CMPLX_INPUT){
+		return micRight_cmplx_input;
+	}
+	else if (name == FRONT_CMPLX_INPUT){
+		return micFront_cmplx_input;
+	}
+	else if (name == BACK_CMPLX_INPUT){
+		return micBack_cmplx_input;
+	}
+	else if (name == LEFT_OUTPUT){
+		return micLeft_output;
+	}
+	else if (name == RIGHT_OUTPUT){
+		return micRight_output;
+	}
+	else if (name == FRONT_OUTPUT){
+		return micFront_output;
+	}
+	else if (name == BACK_OUTPUT){
+		return micBack_output;
+	}
+	else{
+		return NULL;
+	}
+}
