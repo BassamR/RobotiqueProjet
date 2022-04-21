c7f0fcb4c56999a90b61fa18618f497393bfd403
Aloksha2001 <ali.elmorsy@epfl.ch>
2022-04-21 19:44:48 +0200

    added pid and created siren source file

--- ProjetCodeSource/CodeSource/main.c
+++ ProjetCodeSource/CodeSource/main.c
@@ -16,10 +16,11 @@
 #include <communications.h>
 #include <arm_math.h>
 
-#include <spi_comm.h> //works even without that
-#include <leds.h>
+
 #include <sensors/VL53L0X/VL53L0X.h>
 #include <audio/audio_thread.h>
+#include <PID_regulator.h>
+#include <siren.h>
 
 #define sensitivity 20
 #define max_count 250
@@ -38,37 +39,6 @@ static void serial_start(void)
 
 enum state {Detect,Chase};
 
-
-static bool siren_freq=1; //start at 1 with the low freq
-static THD_WORKING_AREA(waRGBThd, 128);
-static THD_FUNCTION(RGBThd, arg) {
-	chRegSetThreadName("RGB leds thread");
-	(void)arg;
-	set_rgb_led(LED4,RGB_MAX_INTENSITY,0,0);  //open the red leds first to create a delay
-	set_rgb_led(LED8,RGB_MAX_INTENSITY,0,0);
-	while(1){
-		siren_freq=!siren_freq;
-		dac_stop();
-		dac_start();
-		toggle_rgb_led(LED2, BLUE_LED,RGB_MAX_INTENSITY);
-		toggle_rgb_led(LED4, RED_LED,RGB_MAX_INTENSITY);
-		toggle_rgb_led(LED6, BLUE_LED,RGB_MAX_INTENSITY);
-		toggle_rgb_led(LED8, RED_LED,RGB_MAX_INTENSITY);
-
-		if (siren_freq==false){
-			dac_play(705);
-		}
-		else{
-			dac_play(933);
-		}
-
-		//uint16_t freq_buffer[2]={705,933};
-		//dac_play_buffer(freq_buffer,2,1,NULL);
-		chThdSleepMilliseconds(500); //make it yield?
-	}
-}
-
-
 int main(void){
 
     halInit();
@@ -79,14 +49,11 @@ int main(void){
     serial_start();
     //starts the USB communication
     usb_start();
-    //inits the motors
-    motors_init();
-    //start spi communication for the rgb leds
+    //starts spi communication (for RGB leds)
     spi_comm_start();
     //start the ToF
     VL53L0X_start();
-    //start the speakers
-    dac_start();
+
 
     while (true){
 
@@ -108,9 +75,7 @@ int main(void){
     		}else if (distance>=dist_to_perp+20){
     			if (count<=250) { //case object was fast \\pbl is this will acivate if the reference moves can make it more robust
     				//call function to estimate speed then activate the lights
-    				chprintf((BaseSequentialStream *)&SD3, "%u \r\n", count);
-    				chThdCreateStatic(waRGBThd, sizeof(waRGBThd), NORMALPRIO, RGBThd, NULL);
-    				// here we should break from the while loop because the detection state will be over start chase state
+    				// can activate all the chase threads here too still works, maybe we wont need the chase state stuff after all
     				current_state=Chase;
     			}
     			//reference=distance;
@@ -119,8 +84,14 @@ int main(void){
     		}
     	}
 
+    	//start the siren thread
+    	siren_start();
+    	//start the pid thread
+    	pid_regulator_start(); //careful where you place this, it should be called only once otherwise panics
+
     	while (current_state==Chase){
     		chprintf((BaseSequentialStream *)&SD3, "%nChase mode is active \r\n");
+
     	}
     }
 }
@@ -136,15 +107,9 @@ uint16_t set_ref(void){  //you need to skip a bunch of measurments at startup be
 	uint16_t ref = 0;
 	for (int i=0; i<=1000;++i){
 		ref= VL53L0X_get_dist_mm();
-		// how to use the dsp instructions??
 		chprintf((BaseSequentialStream *)&SD3, "%u \r\n", ref); //need to uncomment this line for ref to keep that final value for some weird reason
 	} //run 10buffer cycles
-	//uint16_t error= 10;
-	//while(abs(ref-VL53L0X_get_dist_mm())<= error) {
-		//ref=VL53L0X_get_dist_mm();
 
-	//}
-	//chprintf((BaseSequentialStream *)&SD3, "%u \r\n", ref);
 	return ref;
 }
 
