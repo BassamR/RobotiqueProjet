5e675b6dcbe68aa4b25a78a9cf17bfd9be47e1d3
Bassam El Rawas <bassamr99@gmail.com>
2022-05-02 11:57:24 +0200

    Change variable names, merge the two PIDs, replace rotation PID with something else

--- ProjetCodeSource/CodeSource/PID_regulator.c
+++ ProjetCodeSource/CodeSource/PID_regulator.c
@@ -10,35 +10,42 @@
 #include <audio_processing.h>
 
 #define ROTATION_COEFF 				0.4
-#define ROTATION_ERROR_THRESHOLD	3 // degrees
+#define ROTATION_ERROR_THRESHOLD	6 // degrees
 #define ERROR_THRESHOLD 			0.25 //minimum error we want to detect is 0.2cm
 
 #define TS 0.01		// sampling period in s, either multiply with ts or with the variable time
-#define TF 0.05	// filter frequency, in the range 2*ts - 10*ts
+#define TF 0.05		// filter frequency, in the range 2*ts - 10*ts
 
 //PID variables for distance regulation
-#define kp 1.2f
-#define ki 0.0045f
-#define reference 10 // cm
+#define DISTANCE_KP 1.2f
+#define DISTANCE_KI 0.0045f
+#define DISTANCE_REF 10 // cm
 #define U_MAX  13
 
-static float ui = 0; // initial condition for integrator term
-static float up = 0;
+static int16_t distanceSpeed = 0;
+static float distanceError = 0; // = ref - distance, here the declaration = initial error
+
+static float distanceUi = 0; // initial condition for integrator term
+static float distanceUp = 0;
 
 // PID variables for angle regulation
 #define ANGLE_KP 1.9f
-#define ANGLE_KI 0.0045f
-#define ANGLE_KD 0.006f
+#define ANGLE_KI 0.0005f
+#define ANGLE_KD 0.002f
 #define ANGLE_REF 90 //deg
 #define ANGLE_UMAX 13
 
+static int16_t angleSpeed = 0; //what the rotational PID corrects
+static float angleError = 0; // = ref - distance, here the declaration = initial error
+static float prevAngleError = 0;
+
 static float angleUi = 0;
 static float angleUp = 0;
-static float angleUd = 0;
-static float prevAngleError = 0;
+//static float angleUd = 0;
 
 static int count = 0;
 
+// Thread declaration
 
 static THD_WORKING_AREA(waPiRegulator, 256);
 static THD_FUNCTION(PiRegulator, arg) {
@@ -47,12 +54,10 @@ static THD_FUNCTION(PiRegulator, arg) {
 
     systime_t time;
 
-    float angleSpeed = 0; //what the rotational PID corrects
-    float angleError = 0; // = ref - distance, here the declaration = initial error
-
     while(1) {
         time = chVTGetSystemTime();
 
+        // Rotation PID
         float angle = getAngleFromSource();
         angleError = angle - ANGLE_REF;
 //		if(count == 10) {
@@ -62,27 +67,50 @@ static THD_FUNCTION(PiRegulator, arg) {
 //			++count;
 //		}
 
+//        if(fabs(angleError) < ROTATION_ERROR_THRESHOLD) {
+//        	angleSpeed = 0;
+//        } else {
+//        	// Calculate angleUp
+//            angleUp = ANGLE_KP*angleError;
+//            // Calculate angleUd
+//            //angleUd = (TF*angleUd + ANGLE_KD * (angleError - prevAngleError))/(TF + TS); //instead of ts, use real system time maybe
+//            // Calculate angleUi with an ARW
+//            if(!(fabs(angleUp + angleUi + ANGLE_KI*angleError) > ANGLE_UMAX)) angleUi = angleUi + ANGLE_KI*angleError;
+//
+//            //angleSpeed = angleUp + angleUd + angleUi; // loop
+//            angleSpeed = angleUp + angleUi; // loop
+//            //angleSpeed = angleUp;
+//            prevAngleError = angleError;
+//
+//            //avoid command saturation
+//            if(angleSpeed > U_MAX) angleSpeed = U_MAX;
+//            if(angleSpeed < -U_MAX) angleSpeed = -U_MAX;
+//        }
+//        //chprintf((BaseSequentialStream *)&SDU1, "%nspeed(cm)=%.2f \r\n", speed);
+//        angleSpeed = angleSpeed * 1000/13; //convert from cm/s to steps/s
+
         if(fabs(angleError) < ROTATION_ERROR_THRESHOLD) {
         	angleSpeed = 0;
         } else {
-        	// Calculate angleUp
-            angleUp = ANGLE_KP*angleError;
-            // Calculate angleUd
-            angleUd = (TF*angleUd + ANGLE_KD * (angleError - prevAngleError))/(TF + TS); //instead of ts, use real system time maybe
-            // Calculate angleUi with an ARW
-            if(!(fabs(angleUp + angleUi + ANGLE_KI*angleError) > ANGLE_UMAX)) angleUi = angleUi + ANGLE_KI*angleError;
-
-            angleSpeed = angleUp + angleUd + angleUi; // loop
-            prevAngleError = angleError;
-
-            //avoid command saturation
-            if(angleSpeed > U_MAX) angleSpeed = U_MAX;
-            if(angleSpeed < -U_MAX) angleSpeed = -U_MAX;
+        	if(angleError > 0) angleSpeed = U_MAX;
+        	else angleSpeed = -U_MAX;
         }
 
-        //chprintf((BaseSequentialStream *)&SDU1, "%nspeed(cm)=%.2f \r\n", speed);
+        angleSpeed = angleSpeed * 1000/13;
 
-        angleSpeed = angleSpeed * 1000/13; //convert from cm/s to steps/s
+        // Distance PID
+        distanceError = VL53L0X_get_dist_mm()/10 - DISTANCE_REF;
+		if(fabs(distanceError) < ERROR_THRESHOLD) {
+			distanceSpeed = 0;
+		} else {
+			distanceUp = DISTANCE_KP*distanceError;
+			if(!(fabs(distanceUp + distanceUi + DISTANCE_KI*distanceError) > U_MAX)) distanceUi = distanceUi + DISTANCE_KI*distanceError; // corresponds to current ui + ARW
+
+			distanceSpeed = distanceUp + distanceUi; // loop
+		}
+
+		//chprintf((BaseSequentialStream *)&SDU1, "%nspeed(cm)=%.2f \r\n", speed);
+		distanceSpeed = distanceSpeed * 1000/13; //convert from cm/s to steps/s
 
 //        if(count == 10) {
 //        	chprintf((BaseSequentialStream *)&SDU1, "%nAngleSpeed=%.2f \r\n", angleSpeed);
@@ -92,8 +120,8 @@ static THD_FUNCTION(PiRegulator, arg) {
 //        }
 
         //applies the speed from the PI regulator
-		right_motor_set_speed(ROTATION_COEFF*angleSpeed);
-		left_motor_set_speed(-ROTATION_COEFF*angleSpeed);
+		right_motor_set_speed(distanceSpeed + ROTATION_COEFF*angleSpeed);
+		left_motor_set_speed(distanceSpeed - ROTATION_COEFF*angleSpeed);
 
         //100Hz
 		//chprintf((BaseSequentialStream *)&SD3, "%ntime is %d \r\n",chVTGetSystemTime()-time);
@@ -148,6 +176,6 @@ static THD_FUNCTION(PiRegulator, arg) {
 void pid_regulator_start(void){
 	//inits the motors
 	motors_init();
-	chThdCreateStatic(waPiRegulator, sizeof(waPiRegulator), NORMALPRIO+1, PiRegulator, NULL);
+	chThdCreateStatic(waPiRegulator, sizeof(waPiRegulator), NORMALPRIO+2, PiRegulator, NULL);
 }
 
