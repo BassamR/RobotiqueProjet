173bcc81a847c67d1d3c48dd7d3731903484d4ef
Bassam El Rawas <bassamr99@gmail.com>
2022-05-03 20:19:44 +0200

    Get rid of averaging

--- ProjetCodeSource/CodeSource/audio_processing.c
+++ ProjetCodeSource/CodeSource/audio_processing.c
@@ -11,6 +11,8 @@
 #include <fft.h>
 #include <arm_math.h>
 
+#include <motors.h>
+
 #include <math.h>
 
 /*
@@ -23,7 +25,8 @@
 //#define EPUCK_RADIUS    		2.675f  // cm
 #define MIC_DISTANCE    		6.0f  // cm
 #define AMPLITUDE_THRESHOLD		9000
-#define NUMBER_SAMPLES			5		// number of samples for angle average
+#define FREQ_THRESHOLD 			70
+#define NUMBER_SAMPLES			3		// number of samples for angle average
 
 //semaphore
 static BSEMAPHORE_DECL(sendToComputer_sem, TRUE);
@@ -42,8 +45,6 @@ static float micBack_output[FFT_SIZE];
 // Extra static variables and functions
 static int positionInBuffer = 0;
 static int sendToComputer = 0;
-
-static int counter = 0;
 
 /*
 *	Callback called when the demodulation of the four microphones is done.
@@ -98,8 +99,20 @@ void processAudioData(int16_t *data, uint16_t num_samples) {
 //
 //			//call functions that need audio data
 //			int16_t robotAngle = getAngleFromSource();
-//			//alignRobot(robotAngle);
 //			chprintf((BaseSequentialStream *)&SDU1, "%nAngle=%.7d \r\n", robotAngle);
+//
+////			int maxFreqLeft = 0;
+////			float maxLeftOutput = 0;
+////			for(int i = 5; i < FFT_SIZE/2; ++i) {
+////				//start from i=5 as to not consider low freq
+////				if(micLeft_output[i] > maxLeftOutput) {
+////					maxLeftOutput = micLeft_output[i]; //contains the biggest amplitude of the FFT
+////					maxFreqLeft = i; //contains the index at which we have the biggest amplitude
+////				}
+////			}
+////
+////			chprintf((BaseSequentialStream *)&SDU1, "%nFreqIndex=%.7d \r\n", maxFreqLeft);
+//
 //		} else {
 //			++sendToComputer;
 //		}
@@ -118,7 +131,7 @@ void wait_send_to_computer(void){
 *	@params: BUFFER_NAME_t name		microphone name
 *	@return: float*					microphone buffer
 */
-float* get_audio_buffer_ptr(BUFFER_NAME_t name){
+float* get_audio_buffer_ptr(BUFFER_NAME_t name) {
 	if(name == LEFT_CMPLX_INPUT){
 		return micLeft_cmplx_input;
 	}
@@ -155,7 +168,6 @@ float* get_audio_buffer_ptr(BUFFER_NAME_t name){
 *	@return: float angle from source in deg
 */
 int16_t getAngleFromSource(void) {
-	float angleSum = 0;
 	static float prevAngle = 0;
 
 	float maxLeftOutput = 0;
@@ -169,21 +181,20 @@ int16_t getAngleFromSource(void) {
 	float maxBackOutput = 0;
 #endif
 
-	while(counter < NUMBER_SAMPLES) {
-		//get frequency of sound (ie frequency with the highest FFT amplitude)
+	//get frequency of sound (ie frequency with the highest FFT amplitude)
 
-		//run through half the array because FFT gives positive part on [0,512], and negative part on [512, 1024]
-		for(int i = 5; i < FFT_SIZE/2; ++i) {
-			//start from i=5 as to not consider low freq
-			if(micLeft_output[i] > maxLeftOutput) {
-				maxLeftOutput = micLeft_output[i]; //contains the biggest amplitude of the FFT
-				maxFreqLeft = i; //contains the index at which we have the biggest amplitude
-			}
+	//run through half the array because FFT gives positive part on [0,512], and negative part on [512, 1024]
+	for(int i = 5; i < FFT_SIZE/2; ++i) {
+		//start from i=5 as to not consider low freq
+		if(micLeft_output[i] > maxLeftOutput) {
+			maxLeftOutput = micLeft_output[i]; //contains the biggest amplitude of the FFT
+			maxFreqLeft = i; //contains the index at which we have the biggest amplitude
+		}
 
-			if(micRight_output[i] > maxRightOutput) {
-				maxRightOutput = micRight_output[i];
-				maxFreqRight = i;
-			}
+		if(micRight_output[i] > maxRightOutput) {
+			maxRightOutput = micRight_output[i];
+			maxFreqRight = i;
+		}
 
 #ifdef COMPUTE_SIGNED_ANGLE
 			if(micFront_output[i] > maxFrontOutput) maxFrontOutput = micFront_output[i];
@@ -191,57 +202,50 @@ int16_t getAngleFromSource(void) {
 #endif
 		}
 
-		//if the biggest amplitude is smaller than a certain threshold (ie a clear sound isnt being played), do nothing
-		if((maxLeftOutput < AMPLITUDE_THRESHOLD) || (maxRightOutput < AMPLITUDE_THRESHOLD)) {
-			angleSum += prevAngle;
-			++counter;
-			continue; //do not calculate next angle
-		} //this makes the robot memorize current noise angle once noise shuts off
+	//if the biggest amplitude is smaller than a certain threshold (ie a clear sound isnt being played), do nothing
+	if((maxLeftOutput < AMPLITUDE_THRESHOLD) || (maxRightOutput < AMPLITUDE_THRESHOLD)) {
+		return prevAngle;
+	} //this makes the robot memorize current noise angle once noise shuts off
 
-		//choose the frequency coming from the mic closest to the sound source
-		if(maxFreqLeft >= maxFreqRight) maxFreq = maxFreqLeft;
-		else maxFreq = maxFreqRight;
+	//do not take into account frequencies higher than a threshold
+	if((maxFreqLeft > FREQ_THRESHOLD) || (maxFreqRight > FREQ_THRESHOLD)) {
+		return prevAngle;
+	}
 
-		// Calculate time shift at max amplitude frequency using FFT argument
-		float micLeftArg = atan2f(micLeft_cmplx_input[2*maxFreq + 1], micLeft_cmplx_input[2*maxFreq]);
-		float micRightArg = atan2f(micRight_cmplx_input[2*maxFreq + 1], micRight_cmplx_input[2*maxFreq]);
-		float timeShift = 0.00001 * FFT_SIZE * (micRightArg - micLeftArg) / (2 * M_PI * maxFreq); //time difference of arrival;
-		//why do i need to scale by 10^-5 ?
-		//chprintf((BaseSequentialStream *)&SDU1, "%ntimeShift=%.2f \r\n", timeShift);
+	//choose the frequency coming from the mic closest to the sound source
+	if(maxFreqLeft >= maxFreqRight) maxFreq = maxFreqLeft;
+	else maxFreq = maxFreqRight;
 
-		//float timeShift = FFT_SIZE * (micRightArg - micLeftArg) / (2 * M_PI * maxFreq);
+	// Calculate time shift at max amplitude frequency using FFT argument
+	float micLeftArg = atan2f(micLeft_cmplx_input[2*maxFreq + 1], micLeft_cmplx_input[2*maxFreq]);
+	float micRightArg = atan2f(micRight_cmplx_input[2*maxFreq + 1], micRight_cmplx_input[2*maxFreq]);
+	float timeShift = 0.00001 * FFT_SIZE * (micRightArg - micLeftArg) / (2 * M_PI * maxFreq); //time difference of arrival;
+	//why do i need to scale by 10^-5 ?
+	//chprintf((BaseSequentialStream *)&SDU1, "%ntimeShift=%.2f \r\n", timeShift);
 
-		// Calculate angle in deg
-		//float cosineArgument = SOUND_SPEED * timeShift/MIC_DISTANCE;
-		float cosineArgument = SOUND_SPEED * timeShift; //why does this work better????
-		//cap the values of the cosine argument to avoid taking arccos of undefined values
+	//float timeShift = FFT_SIZE * (micRightArg - micLeftArg) / (2 * M_PI * maxFreq);
+
+	// Calculate angle in deg
+	//float cosineArgument = SOUND_SPEED * timeShift/MIC_DISTANCE;
+	float cosineArgument = SOUND_SPEED * timeShift; //why does this work better????
+	//cap the values of the cosine argument to avoid taking arccos of undefined values
 //		if(cosineArgument > 1) cosineArgument = 1;
 //		if(cosineArgument < -1) cosineArgument = -1;
-		//skip next angle calculation if cosineArgument is too big
-		if((cosineArgument > 1) || (cosineArgument < -1)) {
-			angleSum += prevAngle;
-			++counter;
-			continue; //do not calculate next angle
-		}
+	//skip next angle calculation if cosineArgument is too big
+	if((cosineArgument > 1) || (cosineArgument < -1)) {
+		return prevAngle;
+	}
 
-		float angle = acosf(cosineArgument) * 180/M_PI;
-		//prevAngle = angle;
+	float angle = acosf(cosineArgument) * 180/M_PI;
 
 #ifdef COMPUTE_SIGNED_ANGLE
 		//front-back, if negative then sound comes from back, else front
 		if(maxFrontOutput < maxBackOutput) angle = -angle;
 #endif
 
-		angleSum += angle;
-		++counter;
-	}
-
-	float finalAngle = angleSum/NUMBER_SAMPLES;
-	prevAngle = finalAngle;
-	counter = 0;
+	prevAngle = angle;
 
 	//chprintf((BaseSequentialStream *)&SDU1, "%nAngle=%.2f \r\n", angle);
 
-	//return angle;
-	return finalAngle;
+	return angle;
 }
