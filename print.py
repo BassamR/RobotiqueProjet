ad0d4ace7d4e976dbe0b05b2069373fbb7518607
Bassam El Rawas <bassamr99@gmail.com>
2022-05-11 21:32:32 +0200

    Change number of samples for average

--- print.py
+++ print.py
@@ -6,23 +6,20 @@ ser = serial.Serial("COM3", 115200)
 ser.timeout = None
 
 angleArray = []
+numOfSamples = 20
 
 while(1):
     r = ser.readline()
     #r = r[0:len(r)-1] #to get rid of \n
-    print(r)
+    #print(r)
     angleArray.append(int(r))
-    if(len(angleArray) > 2):
+    if(len(angleArray) > numOfSamples+1):
         print("finished")
         break
-
-print("This is the final array")
-print(angleArray)
 
 sum = 0
 for i in range(len(angleArray)):
     sum += angleArray[i]
 
 average = sum/len(angleArray)
-print("This is average")
-print(average)
+print("This is average", average)
