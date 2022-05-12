a882bb6478e48440c5bac990e2d4d239b19e0ac0
Bassam El Rawas <bassamr99@gmail.com>
2022-05-12 10:36:36 +0200

    Get rid of useless files and includes

--- VoleurCodeSource/Noisy/main.c
+++ VoleurCodeSource/Noisy/main.c
@@ -11,54 +11,16 @@
 
 #include <motors.h>
 #include <audio/audio_thread.h>
-
-#include <communications.h>
-#include <arm_math.h>
-
 #include <selector.h>
 
 #define EPUCK_SPEED_FAST 	8 // cm/s
 #define EPUCK_SPEED_SLOW 	5 // cm/s
-
-static void serial_start(void) {
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
-
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
-
-static void timer12_start(void) {
-    //General Purpose Timer configuration   
-    //timer 12 is a 16 bit timer so we can measure time
-    //to about 65ms with a 1Mhz counter
-    static const GPTConfig gpt12cfg = {
-        1000000,        /* 1MHz timer clock in order to measure uS.*/
-        NULL,           /* Timer callback.*/
-        0,
-        0
-    };
-
-    gptStart(&GPTD12, &gpt12cfg);
-    //let the timer count to max value
-    gptStartContinuous(&GPTD12, 0xFFFF);
-}
 
 int main(void) {
 
     halInit();
     chSysInit();
     mpu_init();
-
-    //starts the serial communication
-    serial_start();
-    //starts the USB communication
-    usb_start();
-    //starts timer 12
-    timer12_start();
 
     //inits the motors
     motors_init();
